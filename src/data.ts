b6eea7b73018f566f0ee174b0f317ad6d21700dc
Joshua Ohlman <ohlman@hey.com>
2022-06-23 09:36:59 -0400

    Switch to compact format

--- src/data.ts
+++ src/data.ts
@@ -1,11 +1,13 @@
-import fs from 'fs';
+// import fs from 'fs';
 
 // const data = fs.readFileSync(__dirname + '/lemmas.json', 'utf8');
 
 // export { bible }
 
-import lemmas from './lemmas.json'
+// import lemmas from './lemmas.json'
+
+import compact from './compact.json'
 
 // const lemmas = JSON.parse(data)
 
-export { lemmas };
+export { compact };
